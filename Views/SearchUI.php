b45854d7e307008f4b78891f570f33af291d5e3b
hbdoy <felicity860128@gmail.com>
2017-12-31 16:50:26 +0800

    1. 首次提交(下載後端) 2. 修改bootstrap 3->4 3. 修改navBar 4. 修改查詢清單

---
+++ Views/SearchUI.php
@@ -0,0 +1,55 @@
+<?php
+  include 'header.php';
+  if ( ! isset($_SESSION['uID']) or $_SESSION['uID'] <= 0) {
+    header("Location: ../Views/loginForm.php");
+    exit(0);
+  }
+?>
+
+<head>
+  <meta charset="UTF-8" />
+  <title>查詢清單</title>
+</head>
+<body>
+  <div class="container mt-3">
+    <form action="SearchResult.php" method="post">
+      <div class="form-group row">
+        <label for="keyword" class="col-md-2 col-form-label">關鍵字</label>
+        <div class="col-md-10">
+          <input type="text" name="keyword" class="form-control" id="keyword" placeholder="請輸入關鍵字">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="label" class="col-md-2 col-form-label">物種</label>
+        <div class="col-md-10">
+          <select class="form-control" name="label" id="label">
+            <option value="frog" selected>青蛙</option>
+            <option value="lepidoptera">蝴蝶</option>
+          </select>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="family" class="col-md-2 col-form-label">科別</label>
+        <div class="col-md-10">
+          <input type="text" name="family" class="form-control" id="family" placeholder="請輸入科別">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="genus" class="col-md-2 col-form-label">屬</label>
+        <div class="col-md-10">
+          <input type="text" name="genus" class="form-control" id="genus" placeholder="請輸入屬">
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary">查詢</button>
+        </div>
+      </div>
+    </form>
+  </div>
+
+
+
+<?php
+  include 'footer.php';
+?>
---
+++ Views/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="../Views/index.php">
+    <img src="../img/frog.png" alt="forg_icon" width="32px">
+  </a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="../Views/userinfo.php">使用者清單</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../Views/SearchView.php">查看資料庫</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../Views/SearchUI.php">查詢清單</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href='loginForm.php'>
+          <?php
+          session_start();
+          if (isset($_SESSION['uID']) && $_SESSION['uID'] == true) {
+            echo "登出";
+          } else {
+            echo "登錄";
+          }
+          ?>
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<body>
