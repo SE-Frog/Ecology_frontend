d5a9e11b148cfe9be06aab97c47b6c6c5c56fdc7
IJou <ijou86218@gmail.com>
2018-01-02 22:11:14 +0800

    修改欄位

--- Views/SearchView_edit.php
+++ Views/SearchView_edit.php
@@ -29,19 +29,21 @@
 ?>
 
 <div class="container">
-  <b>edit ecology of&nbsp;<?php echo $edit_id;?></b>
+  <b>Edit ecology of&nbsp;<?php echo $edit_id;?></b>
   <hr/>
   <form method="post" action="../Control/Control.php?act=updateEcology">
-    <b>ID:</b><input type="text" name="dataid" id="dataid" value="<?php echo $edit_id;?>"/><br/>
-    <b>Organ:</b><input type="text" name="organismname" id="organismname" value="<?php echo $Organ;?>"/><br/>
-    <b>Label:</b><input type="text" name="label" id="label" value="<?php echo $Label;?>"/><br/>
-    <b>Family:</b><input type="text" name="family" id="family" value="<?php echo $Family;?>"/><br/>
-    <b>Genus:</b><input type="text" name="genus" id="genus" value="<?php echo $Genus;?>"/><br/>
-    <b>Food:</b><input type="text" name="food" id="food" value="<?php echo $Food;?>"/><br/>
-    <b>Season:</b><input type="text" name="season" id="season" value="<?php echo $Season;?>"/><br/>
-    <b>Status:</b><input type="text" name="status" id="status" value="<?php echo $Status;?>"/><br/>
-    <b>Habitat:</b><input type="text" name="habitat" id="habitat" value="<?php echo $Habitat;?>"/><br/>
-    <b>Note:</b><input type="text" name="note" id="note" value="<?php echo $Note;?>"/><br/>
+    <b>ID:</b><?php echo $edit_id;?><br/>
+    <b>生物名稱:</b><input type="text" name="organismname" id="organismname" value="<?php echo $Organ;?>"/><br/>
+    <b>物種:</b><input type="text" name="label" id="label" value="<?php echo $Label;?>"/><br/>
+    <b>科:</b><input type="text" name="family" id="family" value="<?php echo $Family;?>"/><br/>
+    <b>屬:</b><input type="text" name="genus" id="genus" value="<?php echo $Genus;?>"/><br/>
+    <b>食物:</b><input type="text" name="food" id="food" value="<?php echo $Food;?>"/><br/>
+    <b>季節:</b><input type="text" name="season" id="season" value="<?php echo $Season;?>"/><br/>
+    <b>外觀:</b>
+    <textarea name="status" type="text" id="status" cols="50" rows="5" style="vertical-align:top"><?php echo $Status;?></textarea><br/>
+    <b>習性:</b>
+    <textarea name="habitat" type="text" id="habitat" cols="50" rows="5" style="vertical-align:top"><?php echo $Habitat;?></textarea><br/>
+    <b>註記:</b><input type="text" name="note" id="note" value="<?php echo $Note;?>"/><br/>
     <input type="submit" name="Submit" value="送出" />[<a href='../Views/SearchView.php'>返回</a>]
   </form>
 </div>
