7334161b8706130c4d344a93e0990df27d8e2188
IJou <ijou86218@gmail.com>
2018-01-03 00:31:14 +0800

    beautify

--- Views/SearchView_add.php
+++ Views/SearchView_add.php
@@ -9,24 +9,77 @@
 /* 先針對post進來的id，再以id與資料庫比對，存取資料庫的值到變數中 */
 ?>
 
-<div class="container">
-  <b>Add Ecology</b>
-  <hr/>
-  <form method="post" action="../Control/Control.php?act=createEcology">
-    <b>生物名稱:</b><input type="text" name="organismname" id="organismname" /><br/>
-    <b>物種:</b><input type="text" name="label" id="label" /><br/>
-    <b>科:</b><input type="text" name="family" id="family" /><br/>
-    <b>屬:</b><input type="text" name="genus" id="genus" /><br/>
-    <b>食物:</b><input type="text" name="food" id="food" /><br/>
-    <b>季節:</b><input type="text" name="season" id="season" /><br/>
-    <b>外觀:</b>
-    <textarea name="status" type="text" id="status" cols="50" rows="5" style="vertical-align:top"></textarea><br/>
-    <b>習性:</b>
-    <textarea name="habitat" type="text" id="habitat" cols="50" rows="5" style="vertical-align:top"></textarea><br/>
-    <b>註記:</b><input type="text" name="note" id="note" /><br/>
-    <input type="submit" name="Submit" value="送出" />[<a href='../Views/SearchView.php'>返回</a>]
-  </form>
-</div>
+<head>
+  <meta charset="UTF-8" />
+  <title>新增資料</title>
+</head>
+<body>
+  <div class="container mt-3">
+    <h2 class="mb-3">Add Ecology</h2>
+    <form method="post" action="../Control/Control.php?act=createEcology">
+      <div class="form-group row">
+        <label for="organismname" class="col-md-2 col-form-label">生物名稱</label>
+        <div class="col-md-10">
+          <input type="text" name="organismname" class="form-control" id="organismname"/>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="label" class="col-md-2 col-form-label">物種</label>
+        <div class="col-md-10">
+          <input type="text" name="label" class="form-control" id="label"/>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="family" class="col-md-2 col-form-label">科</label>
+        <div class="col-md-10">
+          <input type="text" name="family" class="form-control" id="family"/>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="genus" class="col-md-2 col-form-label">屬</label>
+        <div class="col-md-10">
+          <input type="text" name="genus" class="form-control" id="genus"/>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="food" class="col-md-2 col-form-label">食物</label>
+        <div class="col-md-10">
+          <input type="text" name="food" class="form-control" id="food"/>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="season" class="col-md-2 col-form-label">季節</label>
+        <div class="col-md-10">
+          <input type="text" name="season" class="form-control" id="season"/>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="status" class="col-md-2 col-form-label">外觀</label>
+        <div class="col-md-10">
+          <textarea name="status" class="form-control" rows="3" id="status"></textarea>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="habitat" class="col-md-2 col-form-label">習性</label>
+        <div class="col-md-10">
+          <textarea name="habitat" class="form-control" rows="3" id="habitat"></textarea>
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="note" class="col-md-2 col-form-label">註記</label>
+        <div class="col-md-10">
+          <input type="text" name="note" class="form-control" id="note"/>
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col-12">
+          <input type="submit" name="Submit" class="btn btn-primary" value="送出" />
+          <input type="button" value="返回" class="btn btn-primary" onclick="location.href='../Views/SearchView.php'">
+        </div>
+      </div>
+    </form>
+  </div>
+</body>
 <?php
   include 'footer.php';
 ?>
