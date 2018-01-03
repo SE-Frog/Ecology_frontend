c8f8ec0e72778f5d2063c98ae1b05e2223142fe2
BETTY_LIN\bettylin <bettysmileha@gmail.com>
2018-01-03 16:38:39 +0800

    登入畫面

--- Views/loginForm.php
+++ Views/loginForm.php
@@ -7,15 +7,39 @@
   // 在header裡面會重新開啟
   include 'header.php';
 ?>
-<div class="container">
-  <h1>Login Form</h1><hr />
-  <form method="post" action="../Control/loginControl.php">
-    <input type="hidden" name="act" value="login">
-    User Name: <input type="text" name="id"><br />
-    Password : <input type="password" name="pwd"><br />
-    <input type="submit">
-  </form>
+<style>
+.myclass {
+  background-image   : url(frogs.png),
+                       url(house.jpg);
+                       
+  background-repeat  : no-repeat,
+                       no-repeat;
+
+  background-size: cover,
+                   cover; 
+}
+form {position absolute; width:400px;}
+.form-group{margin: 50px auto;text-align:center}
+</style>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<body class="myclass"> 
+<div style="position:relative; left:35vw; top:15vw;">
+<form  method="post" action="../Control/loginControl.php" class="rounded" style="background-color:rgba(0,0,0,0.3);">
+  <input type="hidden" name="act" value="login">
+  <div class="form-group">
+    <span style="color:white;font-weight:bold;">UserName</span>
+    <input type="text" name="id" class="form-control" id="exampleInputUserName" placeholder="Enter UserName">
+  </div>
+  <div class="form-group">
+    <span style="color:white;font-weight:bold;">Password</span>
+    <input type="password" name="pwd" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-success" value="登入" >Login</button>
+
+</form>
 </div>
+</div>
+</body>
 
 <?php
   include 'footer.php';
