091b508c45292b5bce32343b697d68c3ffac4cb9
BETTY_LIN\bettylin <bettysmileha@gmail.com>
2018-01-04 18:31:03 +0800

    登入畫面

--- Views/loginForm.php
+++ Views/loginForm.php
@@ -17,14 +17,29 @@
 
   background-size: cover,
                    cover; 
+  background-position:    
 }
 form {position absolute; width:400px;}
 .form-group{margin: 50px auto;text-align:center}
 </style>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 <body class="myclass"> 
-<div style="position:relative; left:35vw; top:15vw;">
-<form  method="post" action="../Control/loginControl.php" class="rounded" style="background-color:rgba(0,0,0,0.3);">
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      
+    </div>
+    <div class="col-6">
+      
+    </div>
+    <div class="col">
+      
+    </div>
+  </div>
+
+<div size="30" style="position:relative;left:100px;top:15vw;">
+<form  method="post" action="../Control/loginControl.php" class="rounded" style=" background-color:rgba(0,0,0,0.3);">
   <input type="hidden" name="act" value="login">
   <div class="form-group">
     <span style="color:white;font-weight:bold;">UserName</span>
